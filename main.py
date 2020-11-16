dc3ce6f319b1bfd5a05a74a5295fd57897b784a9
Dominik Streicher <office@softzilla.at>
2020-11-16 22:03:33 +0100

    Config added

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-echo "test"
+import urllib.request
+import requests
+from bs4 import BeautifulSoup
+import re
+from datetime import date
+import os.path
+from os import path
+import json
+
+
+def loadConfig():
+    with open('config.json') as json_file:
+        return json.load(json_file)
+
+def main():
+    a = loadConfig()
+    print(a[1])
+
+
+if __name__ == "__main__":
+    main()
