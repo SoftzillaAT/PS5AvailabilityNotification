f8cca3c611fdb2af3fafdded8935fc2f52192aa4
Dominik Streicher <office@softzilla.at>
2020-11-17 22:38:21 +0100

    Compare implemented

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
-import urllib.request
+import sys
 import requests
-from bs4 import BeautifulSoup
-import re
-from datetime import date
 import os.path
 from os import path
 import json
-
 
 def loadConfig():
     with open('config.json') as json_file:
         return json.load(json_file)
 
+def sendNotification(url, pattern = ""):
+    print("CHANGE IN URL:  %s" % (url))
+
+def writeFile(fileName, text):
+    f = open(fileName, "w")
+    f.write(text)
+    f.close()
+
+def checkPattern(url, pattern):
+    req = requests.get(url)
+    page = req.text
+    #writeFile("test.html", page)
+    if (page.find(pattern) < 0):
+        print("Pattern not found")
+    else:
+        print("Pattern found")
+
+def checkFileChange(url, file_name):
+    req = requests.get(url)
+    page = req.text
+
+    if (not os.path.isfile(file_name)):
+        writeFile(file_name, page)
+    else:
+        my_file = open(file_name, "r")
+        file_content = my_file.read()
+        if (page.strip() == file_content.strip()):
+            print("Same file")
+        else:
+            sendNotification(url)
+            writeFile(file_name, page)
+
+
 def main():
     a = loadConfig()
-    print(a[1])
 
+    for website in a:
+        print(website)
+        if ("pattern" in website):
+            checkPattern(website['url'], website['pattern'])
+        else:
+            checkFileChange(website['url'], website['file'])
 
 if __name__ == "__main__":
     main()
